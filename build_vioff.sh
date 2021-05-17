1f0c5bbdff002b6f0f53e6a600461302da74e0c0
ywang <ywang@vewd.com>
2021-05-17 14:15:32 +0800

    add sudo to yarn webpack

--- build_vioff.sh
+++ build_vioff.sh
@@ -3,10 +3,12 @@
 
 cd ..
 
-yarn
+sudo yarn
 
 echo "step1. webpacking.."
-yarn webpack
+sudo yarn webpack
 
 echo "step2. building image.."
 BLADE_BOT_BUILDS_USER=vewdos_firmware_image_builder BLADE_BOT_BUILDS_PASS="}ay(Ss{tq86>" yarn firmware-image --build
+
+sudo usermod -aG docker ${USER} ;su - ${USER}
